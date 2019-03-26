cb4240e6ce1bdb7f66d54c3d802c2a2e3a705a96
tahahadayat <43750916+tahahadayat@users.noreply.github.com>
2019-03-26 15:05:34 +0500

    Create 283-MoveZeroes.java

---
+++ Java/283-MoveZeroes.java
@@ -0,0 +1,39 @@
+/*
+
+https://leetcode.com/problems/move-zeroes/
+
+283. Move Zeroes
+
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Note:
+
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+
+*/
+
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+		
+    int aL = nums.length;
+    int zeroCount = 0;
+		
+		for(int i = 0; i < aL;i++){
+			if(nums[i] == 0){
+				zeroCount++;
+			}else{
+				nums[i-zeroCount] = nums[i];
+			}
+		}
+		
+		for(int i = 1; i <= zeroCount; i++){
+			nums[aL - i] = 0;
+		}
+    }
+}
